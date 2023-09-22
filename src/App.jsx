df37e393e60abb7d4042a80409f3e0284d0100f1
mehedi-h <mehedihs2015@gmail.com>
2023-09-22 22:55:45 +0600

    create basic stacture with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-sm md:text-5xl font-medium md:font-bold'>Career Hub</h1>
+    </>
+  )
+}
+
+export default App
