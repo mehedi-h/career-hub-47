f688c4635019d53c8a62ca8e6acdf9e63b4c34ac
mehedi-h <mehedihs2015@gmail.com>
2023-09-24 01:36:00 +0600

    modified

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -55,22 +55,26 @@ const AppliedJobs = () => {
             <ul>
                 {
                     displayJobs.map(job => <li key={allApplyJobs.id}>
-                        <div className="grid md:grid-cols-4">
+                        <div className="grid md:grid-cols-4 md:my-10 border border-gray-200 rounded-md">
                             <div>
                                 <img src={job.logo} alt="" />
                             </div>
                             <div className="md:col-span-3">
-                                <div className="md:flex justify-between">
+                                <div className="md:flex md:px-6 justify-between items-center">
                                     <div>
-                                    <h2>{job.job_title}</h2>
-                                    <h2>{job.company_name}</h2>
-                                    <div>
-                                        <h3>{job.remote_or_onsite}</h3>
-                                        <h3>{job.job_type}</h3>
-                                    </div>
+                                        <h2 className="md:my-3 md:font-bold md:text-lg">{job.job_title}</h2>
+                                        <h2 className="md:font-medium md:text-lg">{job.company_name}</h2>
+                                        <div className="flex gap-2 md:my-3">
+                                            <h3 className=" text-blue-500 border-2 border-blue-500  rounded-md md:px-3 md:py-1 text-xs md:text-base font-normal md:font-semibold">{job.remote_or_onsite}</h3>
+                                            <h3 className="text-blue-500 border-2 border-blue-500  rounded-md md:px-3 md:py-1 text-xs md:text-base font-normal md:font-semibold">{job.job_type}</h3>
+                                        </div>
+                                        <div className="flex gap-5">
+                                            <h3 className="text-xs md:text-base font-normal md:font-semibold">{job.location}</h3>
+                                            <h3 className="text-xs md:text-base font-normal md:font-semibold">{job.salary}</h3>
+                                        </div>
                                     </div>
                                 <div>
-                                    <Link><button>View Details</button></Link>
+                                    <Link><button className="py-2 px-3 md:py-2.5 md:px-4 rounded-md bg-indigo-600 text-white text-xs font-semibold md:text-base md:font-bold">View Details</button></Link>
                                 </div>
                                 </div>
                             </div>
